94340a24910fd091460ebf8e274baa3ef175ec84
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-09-18 17:06:34 -0400

    Defined many play tests

--- Calculator.Unity/Assets/Calculator/UI/Tests/UITests.cs
+++ Calculator.Unity/Assets/Calculator/UI/Tests/UITests.cs
@@ -15,6 +15,12 @@ public class UITests
     private readonly string RESULT_DISPLAY_NAME = "ResultDisplay";
     private readonly string MANAGER_NAME = "Calculator";
 
+    private readonly string ADD_BUTTON_NAME = "AddButton";
+    private readonly string SUBTRACT_BUTTON_NAME = "SubtractButton"; 
+    private readonly string MULTIPLY_BUTTON_NAME = "MultiplyButton"; 
+    private readonly string DIVIDE_BUTTON_NAME = "DivideButton";
+
+
     private readonly string SCENE_NAME = "Calculator/Scenes/Main";
 
     private ResultDisplayManager displayManager;
@@ -22,128 +28,16 @@ public class UITests
     private CalculatorManager calculatorManager;
 
 
-    //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    //// `yield return null;` to skip a frame.
-    //[UnityTest]
-    //public IEnumerator UITestsWithEnumeratorPasses()
-    //{
-    //    this.calculatorManager.Reset();
-    //    const string INITIAL_MESSAGE = "= 0";
-    //    Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
+    private bool referencesSetup = false;
+    private bool settingUpReferences = false;
 
-    //    // Use the Assert class to test conditions.
-    //    // Use yield to skip a frame.
-    //    yield return null;
-    //}
-
-    //[UnityTest]
-    //public IEnumerator UIInitializesProperly()
-    //{
-    //    yield return null;
-    //    yield return null;
-    //    this.SetUp();
-    //    this.calculatorManager.Reset();
-    //    const string INITIAL_MESSAGE = "= 0";
-    //    Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
-
-    //    // Use the Assert class to test conditions.
-    //    // Use yield to skip a frame.
-    //    yield return null;
-    //}
-
-    //[UnityTest]
-    //public IEnumerator LHSUpdatesForNumberPress()
-    //{
-    //    this.calculatorManager.Reset();
-    //    const string INITIAL_MESSAGE = "= 0";
-    //    Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
-
-    //    Click_Number(1);
-    //    Assert.AreEqual(expected: "= 1", actual: this.displayManager.Message);
-
-    //    Click_Number(2);
-    //    Assert.AreEqual(expected: "= 12", actual: this.displayManager.Message);
-
-    //    // Use the Assert class to test conditions.
-    //    // Use yield to skip a frame.
-    //    yield return null;
-    //}
 
 
     [OneTimeSetUp]
     public void OneTimeSetup()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
-        
     }
-
-    [UnityTest]
-    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
-    public IEnumerator UIInitializesProperly()
-    {
-        yield return new WaitWhile(() => !referencesSetup);
-
-        const string INITIAL_MESSAGE = "= 0";
-        Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
-        yield return null;
-    }
-
-    [UnityTest]
-    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
-    public IEnumerator LHSUpdatesForNumberPress()
-    {
-        yield return new WaitWhile(() => !referencesSetup);
-        const string INITIAL_MESSAGE = "= 0";
-        Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
-
-        Click_Number(1);
-        //yield return null;
-        Assert.AreEqual(expected: "= 1", actual: this.displayManager.Message);
-
-        Click_Number(2);
-        Assert.AreEqual(expected: "= 12", actual: this.displayManager.Message);
-
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
-
-    /// <summary>
-    /// Helper to retrieve a button gameobject from our scene
-    /// </summary>
-    /// <param name="number"></param>
-    /// <returns></returns>
-    private static GameObject GetNumberButton(int number)
-    {
-        if (number < 0 || number > 9)
-        {
-            Debug.LogError($"Expecting a value between 0 and 9 but received {number}");
-            return null;
-        }
-        var gameObjectName = $"NumberButton{number}";
-        var gameObject = FindGameObjectInActiveScene(gameObjectName);
-        if (gameObject == null)
-        {
-            Debug.LogError($"Couldn't find gameobject {gameObjectName}");
-            return null;
-        }
-        return gameObject;
-    }
-
-    /// <summary>
-    /// Helper to click a button gameobject in our scene
-    /// </summary>
-    /// <param name="number"></param>
-    private static void Click_Number(int number)
-    {
-        var button = GetNumberButton(number);
-        Debug.Log($"Trying to click {button.name}");
-        button.GetComponent<Button>().onClick.Invoke();
-    }
-
-    private bool referencesSetup = false;
-    private bool settingUpReferences = false;
-
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
@@ -181,11 +75,300 @@ public class UITests
         }
     }
 
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator UIInitializesProperly()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+
+        const string INITIAL_MESSAGE = "= 0";
+        Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
+    }
+
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator LHSUpdatesForNumberPress()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+        const string INITIAL_MESSAGE = "= 0";
+        Assert.AreEqual(expected: INITIAL_MESSAGE, actual: this.displayManager.Message);
+
+        Click_Number(1);
+        //yield return null;
+        Assert.AreEqual(expected: "= 1", actual: this.displayManager.Message);
+
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 12", actual: this.displayManager.Message);
+    }
+
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator Given_LHS_Operator_Functions()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+        
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 12", actual: this.displayManager.Message);
+
+        Click_Operator(ADD_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 12 +", actual: this.displayManager.Message);
+
+        Click_Operator(SUBTRACT_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 12 -", actual: this.displayManager.Message);
+
+        Click_Operator(MULTIPLY_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 12 *", actual: this.displayManager.Message);
+
+        Click_Operator(DIVIDE_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 12 /", actual: this.displayManager.Message);
+    }
+
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator Without_LHS_Operator_Functions()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(ADD_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 +", actual: this.displayManager.Message);
+
+        Click_Operator(SUBTRACT_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 -", actual: this.displayManager.Message);
+
+        Click_Operator(MULTIPLY_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 *", actual: this.displayManager.Message);
+
+        Click_Operator(DIVIDE_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 /", actual: this.displayManager.Message);
+    }
+
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator Given_LHS_And_Operator_RHS_Functions()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(ADD_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 +", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 + 12", actual: this.displayManager.Message);
+        
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(SUBTRACT_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 -", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 - 12", actual: this.displayManager.Message);
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(MULTIPLY_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 *", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 * 12", actual: this.displayManager.Message);
+        
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(DIVIDE_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 /", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 / 12", actual: this.displayManager.Message);
+    }
+
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator Given_LHS_RHS_And_Operator_Equals_Functions()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(ADD_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 +", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 + 12", actual: this.displayManager.Message);
+
+        Click_Equals();
+        Assert.AreEqual(expected: "= 0 + 12 = 12", actual: this.displayManager.Message);
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(SUBTRACT_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 -", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 - 12", actual: this.displayManager.Message);
+
+        Click_Equals();
+        Assert.AreEqual(expected: "= 0 - 12 = -12", actual: this.displayManager.Message);
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(MULTIPLY_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 *", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 * 12", actual: this.displayManager.Message);
+
+        Click_Equals();
+        Assert.AreEqual(expected: "= 0 * 12 = 0", actual: this.displayManager.Message);
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(DIVIDE_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 /", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 / 12", actual: this.displayManager.Message);
+
+        Click_Equals();
+        Assert.AreEqual(expected: "= 0 / 12 = 0", actual: this.displayManager.Message);
+    }
+
+    [UnityTest]
+    [LoadScene("Assets/Calculator/Scenes/Main.unity")]
+    public IEnumerator Given_LHS_And_Operator_And_RHS_Clicking_Operator_Functions()
+    {
+        yield return new WaitWhile(() => !referencesSetup);
+
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(ADD_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 +", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 + 12", actual: this.displayManager.Message);
+
+        Click_Operator(ADD_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 12 +", actual: this.displayManager.Message);
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(SUBTRACT_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 -", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 - 12", actual: this.displayManager.Message);
+        
+        Click_Operator(SUBTRACT_BUTTON_NAME);
+        Assert.AreEqual(expected: "= -12 -", actual: this.displayManager.Message);
+
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(MULTIPLY_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 *", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 * 12", actual: this.displayManager.Message);
+
+        Click_Operator(MULTIPLY_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 *", actual: this.displayManager.Message);
+
+
+        this.calculatorManager.Reset();
+        Assert.AreEqual(expected: "= 0", actual: this.displayManager.Message);
+
+        Click_Operator(DIVIDE_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 /", actual: this.displayManager.Message);
+
+        Click_Number(1);
+        Click_Number(2);
+        Assert.AreEqual(expected: "= 0 / 12", actual: this.displayManager.Message);
+
+        Click_Operator(DIVIDE_BUTTON_NAME);
+        Assert.AreEqual(expected: "= 0 /", actual: this.displayManager.Message);
+    }
+
+    /// <summary>
+    /// Helper to retrieve a button gameobject from our scene
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    private static GameObject GetNumberButton(int number)
+    {
+        if (number < 0 || number > 9)
+        {
+            Debug.LogError($"Expecting a value between 0 and 9 but received {number}");
+            return null;
+        }
+        var gameObjectName = $"NumberButton{number}";
+        var gameObject = FindGameObjectInActiveScene(gameObjectName);
+        if (gameObject == null)
+        {
+            Debug.LogError($"Couldn't find gameobject {gameObjectName}");
+            return null;
+        }
+        return gameObject;
+    }
+
+    /// <summary>
+    /// Helper to click a button gameobject in our scene
+    /// </summary>
+    /// <param name="number"></param>
+    private static void Click_Number(int number)
+    {
+        var button = GetNumberButton(number);
+        Debug.Log($"Trying to click {button.name}");
+        button.GetComponent<Button>().onClick.Invoke();
+    }
+
+    /// <summary>
+    /// Helper to click a button gameobject in our scene
+    /// </summary>
+    /// <param name="number"></param>
+    private static void Click_Operator(string op)
+    {
+        var button = FindGameObjectInActiveScene(op);
+        Debug.Log($"Trying to click {button.name}");
+        button.GetComponent<Button>().onClick.Invoke();
+    }
+
+    /// <summary>
+    /// Helper to click a button gameobject in our scene
+    /// </summary>
+    private static void Click_Equals()
+    {
+        var button = FindGameObjectInActiveScene("EqualsButton");
+        Debug.Log($"Trying to click {button.name}");
+        button.GetComponent<Button>().onClick.Invoke();
+    }
+
+
     private static GameObject FindGameObjectInActiveScene(string name)
     {
         var objects = Resources
-                .FindObjectsOfTypeAll<Transform>()
-                .Where(o => o.gameObject.activeInHierarchy);
+            .FindObjectsOfTypeAll<Transform>()
+            .Where(o => o.gameObject.activeInHierarchy);
         foreach (Transform t in objects)
         {
             if (t.name == name)
@@ -195,7 +378,6 @@ public class UITests
         }
         throw new System.Exception($"Gameobject {name} was not found in the active scene");
     }
-
 }
 
 public class LoadSceneAttribute : NUnitAttribute, IOuterUnityTestAction
