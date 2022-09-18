ef3fc6197f4b6b9dbd324233aa9c70514b785fac
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-09-18 11:37:02 -0400

    dev build script

---
+++ dev/unity_build.ps1
@@ -0,0 +1,30 @@
+param(
+    $unityEditorPath = "C:\Users\amiotj\Desktop\UnityEditors\2020.3.25f1\Editor\Unity.exe" 
+)
+
+
+$project = "Calculator.Unity"
+$namespace = "Calculator.AppManagement.Editor"
+$class = "BuildScripts"
+$method = "sBuildDevCICD"
+
+if ($unityEditorPath -eq $null) {
+    Write-Error "No Unity Editor specified, couldn't build project."
+    exit 1
+}
+
+$arguments = '-quit', '-batchmode', '-logfile -', "-projectPath ${project}", '-buildTarget WindowsStoreApps', "-executeMethod ${namespace}.${class}.${method} -ignoreCompilerErrors"
+
+$unityProcess = Start-Process -NoNewWindow -PassThru -FilePath "$unityEditorPath" -ArgumentList $arguments
+Wait-Process -InputObject $unityProcess
+
+if ($unityProcess.ExitCode -eq 0) {
+    Write-Host "`nBuild successful!"
+} 
+else {
+    Write-Error "`nBuild failed"
+    $msg = $Error[0].Exception.Message
+    Write-Error "`nError Message is: $msg."
+}
+
+exit $LASTEXITCODE
